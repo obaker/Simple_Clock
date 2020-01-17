ac77061a2b868c782e5e5611d534e5da3c8d72c7
obaker <obaker@users.noreply.github.com>
2020-01-17 17:03:34 +0000

    changed playback from mplayer to libVLC

--- clock.py
+++ clock.py
@@ -3,6 +3,11 @@ from tm1637 import TM1637
 from time import time, sleep, localtime
 import RPi.GPIO as GPIO
 import os
+import vlc
+import fnmatch
+import random
+import os.path
+import threading
 DIO=6
 CLK=5
 GPIO.setmode(GPIO.BCM)
@@ -11,32 +16,42 @@ GPIO.setup(10, GPIO.IN)
 GPIO.setup(11, GPIO.IN)
 def show_clock(tm):
     t = localtime()
-    sleep(1 - time() % 1)
     tm.numbers(t.tm_hour, t.tm_min, False)
-tm = TM1637(CLK, DIO)
+tm = TM1637(CLK, DIO)    
 tm.brightness(0)
 radio = False
+source_path="/home/pi/Music/oliver"
+flist = []
+for root, dirs, files in os.walk(source_path, followlinks = True):
+  for name in files:
+    fullname=os.path.join(root,name)
+    if (fnmatch.fnmatch(fullname,'*.mp3') or fnmatch.fnmatch(fullname,'*.flac')):
+      flist.append(fullname)
+instance = vlc.Instance()
+player = instance.media_player_new()
+station = vlc.MediaList(["http://network.absoluteradio.co.uk/core/audio/aacplus/live.pls?service=achq"])
+radio=vlc.Instance().media_list_player_new()
+radio.set_media_list(station)
 while True:
     show_clock(tm)
     if GPIO.input(10) == False:
-      print ("Stop pressed")
-      tm.show("STOP")
-      os.system("killall mplayer")
-      sleep(1)
+       print ("Stop pressed")
+       tm.show("STOP")
+       player.stop()
+       radio.stop()
+       os.system("killall mplayer")
     if GPIO.input(9) == False:
-      print ("Play Pressed")
-      tm.show("Play")
-      if radio == False:
-        os.system("mplayer -playlist /home/pi/alarms/alarmlist.m3u &")
-      if radio == True:
-        os.system("mplayer -playlist http://network.absoluteradio.co.uk/core/audio/aacplus/live.pls?service=achq &")
+       radio.stop()
+       print ("Play Pressed")
+       tm.show("FLAC")
+       player.set_mrl(random.choice(flist))
+       player.play()
     if GPIO.input(11) == False:
-      print ("Mode changed")
-      if radio == False:
-        print ("Radio")
-        radio = True
-        tm.show("RAD ")
-      else:
-        print ("Local Files")
-        radio = False
-        tm.show("FLAC")
+       player.stop()
+       print ("Radio")
+       tm.show("RAD ")
+       radio.play()
+    if player.get_state() == vlc.State.Ended:
+        player.set_mrl(random.choice(flist))
+        player.play()
+    sleep(1)
