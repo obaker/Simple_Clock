021b026ae80671f3467d9b0e8cbb3b3fd2be374a
obaker <obaker@users.noreply.github.com>
2020-01-18 21:13:47 +0000

    Added DB editor (alarm.py) Modified clock.py to read alarms from database Removed mkpl as no longer required

---
+++ alarm.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+import sqlite3
+from time import localtime, strftime
+from sys import exit, argv
+
+def initialise_db():#Creates DB if it doesn't already exist
+  db = sqlite3.connect(db_file)
+  c = db.cursor()
+  c.execute('''CREATE TABLE alarms (alarm_ID INTEGER PRIMARY KEY, time TEXT, dow INT, radio INT);''')
+  db.commit()
+  c.execute('PRAGMA user_version=1;')
+  db.commit()
+  db.close()
+def display_db():#Displays database directly, without any filtering
+   db = sqlite3.connect(db_file)
+   c = db.cursor()
+   c.execute("SELECT * from alarms")
+   rows = c.fetchall()
+   print ("Alarm ID, Time, dow, radio")
+   for row in rows:
+       print(row)
+   db.commit()
+   db.close()
+def view_all():# Shows all alarms in a more human readable format.
+   weekday_numbers = [128, 64, 32, 16, 8, 4, 2]
+   weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+   db = sqlite3.connect(db_file)
+   c = db.cursor()
+   for dow, time, alarm_ID in db.execute("SELECT dow, time, alarm_ID FROM alarms ORDER BY time ASC;"): #Selects the primary key, time and days of week sorted by time, loops for all alarms
+       active_days = []#List to store active days for alarm
+       for i in range(7): # 
+          if dow - weekday_numbers[i] >= 0: #checks what days alarm is active, starting from monday
+             dow = dow - weekday_numbers[i] 
+             active_days.append(weekdays[i]) # adds active days to list
+       print (alarm_ID, time, active_days, end='')#Displays all alarms
+       if dow % 2 == 1:#adds an (R) to repeating alarms
+          print (" (R)")
+       else:#Else if alarm is non repeating
+          print ("\n")
+   
+def view_db(): #Shows todays alarms
+   weekday_numbers = [128, 64, 32, 16, 8, 4, 2]
+   weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+   db = sqlite3.connect(db_file)
+   c = db.cursor()
+   for dow, time in db.execute("SELECT dow, time FROM alarms ORDER BY time ASC;"): #Selects day of week and time, to show the user.
+       active_days = []
+       for i in range(7):
+          if dow - weekday_numbers[i] >= 0:
+             dow = dow - weekday_numbers[i]
+             active_days.append(weekdays[i])
+       if strftime("%A", localtime()) in active_days and strftime("%H:%M", localtime()) < time: #Only processes the alarm if it's set for today, and in the future.
+          print(time, active_days, end='')#Displays all alarms
+          if dow % 2 == 1:#adds an (R) to repeating alarms
+              print (" (R)")
+          else:
+              print ("\n")
+def add_alarm(time, dow, radio):
+    db = sqlite3.connect(db_file)
+    c = db.cursor()
+    c.execute("INSERT INTO alarms VALUES (NULL, '" + time + "' , "+str(dow)+", "+str(radio)+")")#Inserts user specified values into alarm DB
+    db.commit()
+    db.close()
+def remove_alarm():
+    view_all()#Runs the function showing all alarms in a more human readable format.
+    delete = input ("Enter the ID of the alarm to delete\n   ")
+    db = sqlite3.connect(db_file)
+    c = db.cursor()
+    c.execute("DELETE FROM alarms WHERE alarm_ID = "+delete)#Deletes the specified alarm
+    db.commit()
+    db.close()
+if len(argv) > 2:
+   exit()
+if len(argv) < 2:
+   db_file = "/home/pi/alarms/clock.db"
+else:
+   db_file = sys.argv[1]
+db = sqlite3.connect(db_file)
+c = db.cursor()
+c.execute('PRAGMA user_version;')
+db.commit()
+version = c.fetchone()
+db.close()
+if version is None or version[0] == 0:
+   initialise_db()
+if version[0] != 1:
+   print("Incompatible DB version")
+   exit()
+while True:
+   command = input("  (V)iew alarms  \n  (D)isplay Database Directly\n  (A)dd Alarm \n  (R)emove Alarm \n  (Q)uit \n   ").lower()#Shows menu, and waits for valid input
+   if command == "v" or command == "view":
+      view_all()#Display human readable DB
+   if command == "d" or command == "display":
+      display_db() #display DB directly
+   if command == "a" or command == "add":
+      weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+      weekday_numbers = [128, 64, 32, 16, 8, 4, 2]
+      dow = 0
+      alarm_time_hr = input("Please input the hour for the alarm \n   ")
+      alarm_time_min = input("Please input the minutes for the alarm \n   ")
+      if len(alarm_time_hr) == 1:
+         alarm_time_hr = "0" + alarm_time_hr
+      alarm_time = alarm_time_hr + ":" + alarm_time_min
+      for i in range(7):
+         active = input ("Is alarm active on "+weekdays[i] +" (y/N)\n   ").lower()
+         if active == "y" or active == "yes":
+            dow = dow + weekday_numbers[i]
+
+      repeat = input("Does alarm repeat (y/N)\n   ").lower()
+      if repeat == "y" or repeat == "yes":
+         dow = dow + 1;
+      radio_preset = input("Please choose a radio station, input 0 for local music\n   ")
+      if radio_preset == "":
+         radio_preset = 0
+      add_alarm(time = alarm_time, dow = dow , radio = radio_preset )
+   if command == "r" or command == "remove":
+      print ("Remove")
+      remove_alarm()
+   if command == "q" or command == "quit":
+      print ("Quitting...")
+      exit()
+ 
--- clock.py
+++ clock.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from tm1637 import TM1637
-from time import time, sleep, localtime
+from time import time, sleep, localtime, strftime
 import RPi.GPIO as GPIO
 import os
 import vlc
@@ -8,50 +8,124 @@ import fnmatch
 import random
 import os.path
 import threading
+import sqlite3
+from sys import argv, exit
 DIO=6
 CLK=5
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(9, GPIO.IN)
 GPIO.setup(10, GPIO.IN)
 GPIO.setup(11, GPIO.IN)
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+def initialise_db():
+  db = sqlite3.connect(db_file)
+  c = db.cursor()
+  c.execute('''CREATE TABLE alarms (alarm_ID INTEGER PRIMARY KEY, time TEXT, dow INT, radio INT);''')
+  db.commit()
+  c.execute('PRAGMA user_version=1;')
+  db.commit()
+  db.close()
+def remove_alarm(delete):
+    db = sqlite3.connect(db_file)
+    c = db.cursor()
+    c.execute("DELETE FROM alarms WHERE alarm_ID = "+delete)
+    db.commit()
+    db.close()
+def view_db(alarm_time, ID):   
+   time = "00:00"
+   repeat = False
+   alarm_ID = None
+   alarm_set = False
+   db = sqlite3.connect(db_file)
+   c = db.cursor()
+   for dow, time, alarm_ID in db.execute("SELECT dow, time, alarm_ID FROM alarms ORDER BY time ASC;"): #Selects the primary key, time and days of week sorted by time, loops for all alarms
+       active_days = []
+       for i in range(7):
+          if dow & (128 >> i) != 0:
+             #dow = dow - weekday_numbers[i]
+             active_days.append(weekdays[i])
+       if strftime("%A", localtime()) in active_days and strftime("%H:%M", localtime()) < time < alarm_time and alarm_set == False:
+          alarm_set = True
+          alarm_time = time
+          ID = alarm_ID
+          if dow % 2 == 1:
+              repeat = True
+   db.commit()
+   db.close()
+   return (alarm_time, repeat,ID)
 def show_clock(tm):
     t = localtime()
     tm.numbers(t.tm_hour, t.tm_min, False)
+if len(argv) > 2:
+   exit()
+if len(argv) < 2:
+   db_file = "/home/pi/alarms/clock.db"
+else:
+   db_file = sys.argv[1]
 tm = TM1637(CLK, DIO)    
 tm.brightness(0)
 radio = False
 source_path="/home/pi/Music/oliver"
+alarm_path="/home/pi/alarms/symlinks"
 flist = []
 for root, dirs, files in os.walk(source_path, followlinks = True):
   for name in files:
     fullname=os.path.join(root,name)
     if (fnmatch.fnmatch(fullname,'*.mp3') or fnmatch.fnmatch(fullname,'*.flac')):
       flist.append(fullname)
+alist = []
+for root, dirs, files in os.walk(alarm_path, followlinks = True):
+  for name in files:
+    fullname=os.path.join(root,name)
+    if (fnmatch.fnmatch(fullname,'*.mp3') or fnmatch.fnmatch(fullname,'*.flac')):
+      alist.append(fullname)
 instance = vlc.Instance()
 player = instance.media_player_new()
 station = vlc.MediaList(["http://network.absoluteradio.co.uk/core/audio/aacplus/live.pls?service=achq"])
 radio=vlc.Instance().media_list_player_new()
 radio.set_media_list(station)
+count = "-1"
+alarm_time = "25:00"
+ID = 0
+alarm_checker = view_db(alarm_time, ID)
+alarm_time = alarm_checker[0] 
+ID = alarm_checker[2]
 while True:
     show_clock(tm)
     if GPIO.input(10) == False:
-       print ("Stop pressed")
        tm.show("STOP")
        player.stop()
        radio.stop()
-       os.system("killall mplayer")
     if GPIO.input(9) == False:
        radio.stop()
-       print ("Play Pressed")
        tm.show("FLAC")
        player.set_mrl(random.choice(flist))
        player.play()
     if GPIO.input(11) == False:
        player.stop()
-       print ("Radio")
        tm.show("RAD ")
        radio.play()
     if player.get_state() == vlc.State.Ended:
         player.set_mrl(random.choice(flist))
         player.play()
+    if count != strftime("%M", localtime()):
+        alarm_checker = view_db(alarm_time, ID)
+        #print ("Next alarm at " + alarm_checker[0])
+        alarm_time = alarm_checker[0] 
+        ID = alarm_checker[2]
+        #print (ID)
+    if strftime("%H:%M", localtime()) >= alarm_time:
+       #print (alarm_time)
+       alarm_time = "25:00"
+       #print (alarm_checker[2])
+       radio.stop()
+       player.stop()
+       player.set_mrl(random.choice(alist))
+       player.play()
+       if alarm_checker[1] == False: # removes alarm if repeat not set in DB
+          remove_alarm(str(alarm_checker[2]))
+       alarm_checker = view_db(alarm_time, ID)
+       alarm_time = alarm_checker[0] 
+       ID = alarm_checker[2]
+    count = strftime("%M", localtime())
     sleep(1)
